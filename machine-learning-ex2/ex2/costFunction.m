f06c384d9f88f2fa168c5fba55de98d75f24b16a
feiwofeifeixiaowo <feiwofeifeixiaowo@gmail.com>
2017-03-04 22:34:54 +0800

    gradient Descent

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -16,16 +16,30 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+%;
+% Note: grad should have th`e same dimensions as theta
 %
-% Note: grad should have the same dimensions as theta
-%
 
+%  J : computeCost
+% neg = find(y == 0);
 
+h = sigmoid(X*theta);
 
+J = (-1 / m) * (sum(y' * log(h) + (1 - y)' * log(1 - h)));
 
+% miss understand of grad
+% grad is the lose of theta, that every iter.
+% some param for gradient
+alpha     = 0.01;% no need alpha
+num_iters = 1500;% no need iter number
 
-
-
+%{
+for i = 1:num_iters
+  theta = theta - (alpha / m) * X' * (sigmoid(X * theta) - y);
+end
+grad = theta;
+%}
+grad = (1 / m) * X' * (sigmoid(X * theta) - y);
 
 % =============================================================
 
