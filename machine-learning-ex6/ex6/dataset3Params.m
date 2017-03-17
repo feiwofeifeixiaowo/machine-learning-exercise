a8bf701f04fc695fbf56773fe5a6aa43b8afd0eb
F <feiwofeifeixiaowo@gmail.com>
2017-03-17 17:48:10 +0800

    update
    transfer to windows

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,20 +15,44 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+% parameters should try to search
+param = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+x1 = [1 2 1]; x2 = [0 4 -1];
 
+prediction_error = [];
 
+for i = 1:length(param)
+  for j = 1:length(param)
+    C     = param(i);
+    sigma = param(j);
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions       = svmPredict(model, Xval);
+    prediction_error  = [prediction_error ; mean(double(predictions ~= yval)) i j];
+  end
+end
 
+disp("size of prediction_error is :");
+disp(size(prediction_error));
 
+[p_err index] = max(prediction_error(:, 1));
 
+disp("max prediction_error is: ");
+disp(p_err);
+disp("index is :");
+disp(index);
+[p_err i j] = prediction_error(index, :);
+
+C     = i;
+sigma = j;
 % =========================================================================
 
 end
